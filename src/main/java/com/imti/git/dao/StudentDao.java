6d869e53f992de6bf6c060979b47700e40da3387
Git-Project02 <2252150995@qq.com>
2020-04-29 16:42:33 +0800

    develop: 修改Student.java,添加age属性和get/set方法 创建StudentDao.java

---
+++ src/main/java/com/imti/git/dao/StudentDao.java
@@ -0,0 +1,10 @@
+package com.imti.git.dao;
+
+import com.imti.git.pojo.Student;
+
+public class StudentDao {
+
+	public void insertStudent(Student student) {
+		System.out.println("insert Student: " + student);
+	}
+}
--- src/main/java/com/imti/git/pojo/Student.java
+++ src/main/java/com/imti/git/pojo/Student.java
@@ -2,9 +2,11 @@ package com.imti.git.pojo;
 
 public class Student {
 
-	private String name;					
-	//	edit by Git-Project02
-	//	edit by Git-Project01
+	private String name;
+	// edit by Git-Project02
+	// edit by Git-Project01
+
+	private Integer age;
 
 	public String getName() {
 		return name;
@@ -12,6 +14,14 @@ public class Student {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
 	}
 
 }
